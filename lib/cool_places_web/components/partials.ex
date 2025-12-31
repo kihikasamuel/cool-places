d015a34af95b4a132b2043602f38639085affac8
Samuel Kihika <samuel.kihika@valuechainfactory.com>
2025-12-31 18:22:17 +0300

    fix: mobile menu responsiveness

--- lib/cool_places_web/components/partials.ex
+++ lib/cool_places_web/components/partials.ex
@@ -2,4 +2,7 @@ defmodule CoolPlacesWeb.Partials do
   use CoolPlacesWeb, :html
 
   embed_templates "partials/*"
+
+  def is_logged_in?(nil), do: false
+  def is_logged_in?(current_user) when is_map(current_user), do: true
 end
