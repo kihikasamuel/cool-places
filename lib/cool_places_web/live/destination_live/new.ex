1a042c40d58499568d4404f44c437433470f3b4f
Samuel Kihika <samuel.kihika@valuechainfactory.com>
2025-12-31 23:46:31 +0300

    fix: adds listing -- we call it a year

--- lib/cool_places_web/live/destination_live/new.ex
+++ lib/cool_places_web/live/destination_live/new.ex
@@ -4,12 +4,4 @@ defmodule CoolPlacesWeb.DestinationsLive.New do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
-
-  def render(assigns) do
-    ~H"""
-    <div class="flex flex-col gap-4 w-full max-w-2xl mx-auto">
-      <h1>Listing</h1>
-    </div>
-    """
-  end
 end
