b45ed4c9db58b5f2f2ca58c99f83619a65404e82
Samuel Kihika <samuel.kihika@valuechainfactory.com>
2026-01-01 14:09:24 +0300

    adds: display users avatar

--- lib/cool_places_web/components/partials.ex
+++ lib/cool_places_web/components/partials.ex
@@ -3,6 +3,16 @@ defmodule CoolPlacesWeb.Partials do
 
   embed_templates "partials/*"
 
+  @doc """
+  Check if current user is set
+  """
   def is_logged_in?(nil), do: false
   def is_logged_in?(current_user) when is_map(current_user), do: true
+
+  @doc """
+  Checks if a user has set an avatar
+  """
+  def is_avatar_set?(nil), do: false
+  def is_avatar_set?(%{avatar_url: nil} = _current_user), do: false
+  def is_avatar_set?(%{avatar_url: avatar_url} = _current_user) when not is_nil(avatar_url), do: true
 end
