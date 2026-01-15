78ea4cdcc9646bfd7f2e78431bded0c816234625
Samuel Kihika <summitros@gmail.com>
2026-01-15 22:40:08 +0300

    fix: carousel

--- lib/cool_places_web/live/destination_live/show.ex
+++ lib/cool_places_web/live/destination_live/show.ex
@@ -7,16 +7,69 @@ defmodule CoolPlacesWeb.DestinationsLive.Show do
   def mount(_params, _session, socket) do
     socket =
       socket
-      |> assign(is_loading: false, travel_plan: nil)
+      |> assign(
+        is_loading: false,
+        travel_plan: nil,
+        active_image_index: 0,
+        active_destination_asset: nil
+      )
 
     {:ok, socket}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
+    destination = Destinations.get_destination!(id)
+
     {:noreply,
      socket
      |> assign(:page_title, "Destination Details")
-     |> assign(:destination, Destinations.get_destination!(id))}
+     |> assign(:destination, destination)
+     |> assign(:active_destination_asset, destination.destination_asset |> Enum.at(0))}
+  end
+
+  @impl true
+  def handle_event("set_active_image", %{"active_image_index" => active_image_index}, socket) do
+    active_image_index = String.to_integer(active_image_index)
+
+    socket =
+      socket
+      |> set_active_image(active_image_index)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("prev_image", _params, socket) do
+    active_image_index = Kernel.-(socket.assigns.active_image_index, 1)
+
+    socket =
+      socket
+      |> set_active_image(active_image_index)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("next_image", _params, socket) do
+    active_image_index = Kernel.+(socket.assigns.active_image_index, 1)
+
+    socket =
+      socket
+      |> set_active_image(active_image_index)
+
+    {:noreply, socket}
+  end
+
+  defp set_active_image(socket, index) do
+    active_destination_asset = socket.assigns.destination.destination_asset
+
+    if index < 0 or index >= length(active_destination_asset) do
+      socket
+    else
+      socket
+      |> assign(
+        active_image_index: index,
+        active_destination_asset: active_destination_asset |> Enum.at(index)
+      )
+    end
   end
 end
